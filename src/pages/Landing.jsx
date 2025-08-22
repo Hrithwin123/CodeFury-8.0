a034c79d0b6a810ba318e155cf07e66f2eb30f45
Hrithwin <hrithwin123@gmail.com>
2025-08-22 17:54:53 +0530

    added tailwind and react-router

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+export default function Landing() {
+
+    return(
+        <div className="h-[100dvh] w-screen flex items-center justify-center font-extrabold text-7xl">
+            Hello World
+        </div>
+    )
+
+
+}
